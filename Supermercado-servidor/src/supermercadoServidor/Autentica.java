52e471f0737a6d3b7f6eeaf12925f8e52488f93e
jun014 <anderson.morikawa@usp.br>
2015-07-01 22:22:54 -0300

    a

--- Supermercado-servidor/src/supermercadoServidor/Autentica.java
+++
@@ -1,7 +0,0 @@
-package supermercadoServidor;
-
-public interface Autentica {
-	void setLogin(int id, String senha);
-	boolean login(Autentica user, String senha);
-	void setInfo(int id, String nome, String email);
-}
